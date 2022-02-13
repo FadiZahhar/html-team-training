b76fa32fd27ec3ee9148042aa2cfca9f771c1749
lora lameh <loralameh@gmail.com>
2022-02-13 17:43:38 +0200

    intro to debuging functions and variables

---
+++ debugingfunctions&variables.js
@@ -0,0 +1,23 @@
+// document.write('hello world')
+alert('hello from alert')
+console.log('hello from console')
+
+// variable names can only start with letters _ or $
+let firstName = 'lora'
+const lastname = 'lameh'
+//concatination
+console.log(firstName + ' ' + lastname)
+
+// no need to spesify the type of a numbers
+// const num = 34
+// let decimal = 3.5
+
+const num1 = '34'
+let decimal1 = '3.5'
+let value = num1 - decimal1
+console.log(value)
+
+const num2 = '34'
+let decimal2 = '3.5'
+let value2 = num2 + decimal2
+console.log(value2)
