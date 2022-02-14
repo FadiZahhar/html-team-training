260d82a0cf1ad56059990bb115f5729050d4355b
Fatima-kouta <fatimakouta286@gmail.com>
2022-02-14 13:58:29 +0200

    JS HTML DOM tasks

---
+++ javascript/DOM/Css.js
@@ -0,0 +1,179 @@
+//Css
+const random1 =document.querySelector('.Random');
+// random1.style.fontSize='3rem';
+// random1.style.backgroundColor='red'
+random1.classList.add('title');
+
+//select element
+//addEventListener
+//what event,what to do
+const btn=document.querySelector('.btn')
+const h2=document.querySelector('h2')
+
+btn.addEventListener('click',function(){
+ h2.classList.add('red');
+})
+
+
+
+// call back function 
+btn.addEventListener('click',changeColors);
+
+function changeColors(){
+ const hasClass=h2.classList.contains('red');
+ if(hasClass){
+  h2.classList.remove('red');
+ }else{
+  h2.classList.add('red');
+
+ }
+}
+
+//click
+//mousedown -button is pressed
+//mouseup-button is released
+//mouseenter -moved onto an elment
+//mouseleave -moved out of an elment
+
+const btn1=document.querySelector('.yellow')
+const btn2=document.querySelector('.blue')
+const btn3=document.querySelector('.pink')
+
+btn1.addEventListener('mouseup',function(){
+ h2.classList.add('yellow');
+})
+btn2.addEventListener('mouseenter',function(){
+ h2.classList.add('blue');
+})
+btn3.addEventListener('mouseleave',function(){
+ h2.classList.add('pink');
+})
+
+//keypress -when key is pressed
+//keydown -when key is down
+//keyup -when key is up
+
+const inputname=document.querySelector('#press')
+inputname.addEventListener('keypress',function(){
+ console.log('keypress')
+})
+const inputname1=document.querySelector('#down')
+inputname1.addEventListener('keydown',function(){
+ console.log('down')
+})
+
+const inputname2=document.querySelector('#up')
+inputname2.addEventListener('keyup',function(){
+ console.log('up')
+})
+
+
+//event
+//event.currentTarge--always refers to the element to wich the event handler has been attached to
+//event.type
+//prevent()--prevents default behaviour
+const Event=document.querySelector('.event')
+Event.addEventListener('click',function(event){
+ event.currentTarget.classList.add('red');
+console.log(event.currentTarget)//<button class="event red">Click Me</button>
+console.log(event.type)//click
+console.log(this)//<button class="event red">Click Me</button>
+})
+//or
+Event.addEventListener('click',event=>{
+ console.log(event.currentTarget)
+})
+
+//target -identifies the element  on which  the event occured
+const btns=document.querySelectorAll('.Btn')
+btns.forEach(function(btn){
+ btn.addEventListener('click',function(e){
+  e.currentTarget.style.color='green';//color of btn chang
+  console.log(e.currentTarget)//<button class="Btn" style="color: green;">Second<strong style="color: green;">
+  console.log(e.target) //<strong style="color: green;"> button more nested</strong>
+  e.target.style.color='green';//just the elment :
+ })
+})
+
+// allows select dynamic element
+// event propagation : order the events are fired
+// Propagation means bubbling up to parent elements or capturing down to child elements.
+//The stopPropagation() method prevents propagation of the same event from being called.
+
+// event bubbling :clicked element first then bubbles up 
+
+const container=document.querySelector('.container');
+const list=document.querySelector('.list-item');
+const BTN=document.querySelector('.BTN');
+
+function showBubbiling(event){
+ console.log('current target',event.currentTarget)
+ console.log(' target',event.target)
+
+ }
+function stopPropagation(event){
+console.log('you clicked on a list item ')
+event.stopPropagation()
+}
+list.addEventListener('click',showBubbiling);
+container.addEventListener('click',showBubbiling)
+document.addEventListener('click',showBubbiling)
+window.addEventListener('click',showBubbiling)
+
+
+BTN.addEventListener('click',function(){
+ const elment=document.createElement('h1');
+ elment.classList.add('heading')
+ elment.textContent="i'm inside the container";
+ container.appendChild(elment)
+})
+BTN.addEventListener('click',function(e){
+ if(e.currentTarget.classList.contains('heading')){
+  console.log('hello there ')
+ }
+
+})
+
+
+// submit event listener
+// prevent default
+// how to get a value 
+
+const form =document.getElementById('form');
+const name1=document.getElementById('name');
+const password=document.getElementById('password');
+
+form.addEventListener('submit',function(e){
+e.preventDefault();
+console.log(name1.value)
+console.log(password.value)
+})
+
+//storing as a string
+//LocalStorage.setItem('key','value') keep the info in between  the opening and closing the browser 
+//sessionStorage.setItem('key','value') keep the info while  the tab still open
+
+localStorage.setItem('name','john');
+localStorage.setItem('name1','john1');
+const name_s=localStorage.getItem('name');
+console.log(name_s)//john
+
+localStorage.removeItem('name1');
+
+//localStorage.clear()
+
+// storing as an array
+//JSON.stringify,JSON.parse
+const friend=['john','bon','jad']
+const valuess=JSON.parse(localStorage.getItem('friend',JSON.stringify(friend)));
+console.log(valuess[0])//john
+
+let fruits ;
+if(localStorage.getItem('fruits')){
+ fruits=JSON.parse(localStorage.getItem('fruits'));
+}else{
+fruits=[]
+}
+console.log(fruits);
+fruits.push('apple');
+localStorage.setItem('fruits',JSON.stringify(fruits));
--- javascript/DOM/javascript2.js
+++ javascript/DOM/javascript2.js
@@ -28,11 +28,12 @@ console.log(window)
 
 //2- getElementsByTagName('tagname')
 const items=document.getElementsByTagName('li');
-items[1].style.color="red";
+
+items[0].style.color="red";
 const betteritems=[...items]
+
 betteritems.forEach(function(item){
-item.style.backgroundColor="white"
-item.style.color="black"
+item.style.backgroundColor="#aaafce"
 item.style.margin="4px 7px "
 })
 
@@ -40,3 +41,154 @@ item.style.margin="4px 7px "
 document.getElementsByClassName('btn')
 
 //4- querySelector('any css')- selects single 
+document.querySelector('#result').style.backgroundColor='beige'
+document.querySelector('li:last-child').style.backgroundColor='lightblue'
+
+//5- querySelector('any css')- selects single 
+const all=document.querySelectorAll('.special1');
+all.forEach(function(item){
+ item.style.color="blue"
+})
+
+//childNodes:return all childNodes including whitespace whitch is treated as a text nodes
+
+const Result=document.querySelector('#result1');
+//childNodes
+const allchildren=Result.childNodes;
+console.log(allchildren)
+const children=Result.children
+console.log(children)
+//firstChild :return whitespace
+console.log(Result.firstChild)
+//lastChild :return whitespace
+console.log(Result.lastChild)
+
+// parentElment
+const heading=document.querySelector('h3');
+const parent =heading.parentElement;
+parent.style.backgroundColor='red';
+
+//previousSibiling :returns the previous node of the specified node
+//nextSibiling
+const nextSibiling=heading.nextSibling.nextSibling;
+// console.log(nextSibiling)//<h3>hello </h3>
+
+//nextElementSibiling "without whitespace"
+heading.nextElementSibling.style.color="yellow";
+
+//nodeValue
+const item= document.getElementById("nodevalue");
+console.log(item.nodeValue)//null
+console.log(item.firstChild.nodeValue)//Node Value
+console.log(item.childNodes[0].nodeValue)//Node Value
+
+//textContent 
+const textcontent=item.textContent;
+console.log(textcontent)//Node Value
+
+//getAttribute
+// The value of the class attribute of the  element id=result:
+const first=document.querySelector("#result");
+const classvalue=first.getAttribute("class");
+console.log(classvalue)//ul_class
+
+const link=document.getElementById("links");
+const showlink=link.getAttribute("href");
+console.log(showlink)//#
+//setAttribute
+const last=link.nextElementSibling;
+last.setAttribute('class','newclassname');
+last.textContent="i have a class of newclassname ";
+console.log(last)
+//<li class="newclassname" style="background-color: lightblue; margin: 4px 7px;">i have a class of newclassname </li>
+
+
+
+//className :sets or returns an element's class attribute.
+const FIRST=document.getElementById("First");
+console.log(FIRST.className)// class=color 
+//set class  
+const Second=document.getElementById("Second");
+Second.className='second';
+
+
+//classList :returns the CSS classnames of an element.
+
+const third=document.getElementById("Third");
+third.classList.add('colors');
+third.classList.add('text');
+//or
+// third.classList.add('colors','text')
+const ClassValue=third.classList;
+console.log(ClassValue)//DOMTokenList(2) ['colors', 'text', value: 'colors text']
+third.classList.remove('text');
+console.log(third.classList)//DOMTokenList ['colors', value: 'colors']
+
+//we can check if particular element does have that class
+let REsult=third.classList.contains('colors')
+if(Result){
+console.log('hello world ')
+}else{
+ console.log('does not have the class  ')
+
+}
+//1-
+
+//CreateElement ('elemen')
+// createTextNode('text content')
+//element.appendChild(childelement)
+//insertBefore('elemnt','location')
+
+const Element =document.querySelector('#createElement');
+const FiRst=document.querySelector('.red');
+//create empty element 
+const bodyDiv=document.createElement('div');
+
+// create text node
+const text=document.createTextNode('a simple body div');
+bodyDiv.appendChild(text)
+document.body.appendChild(bodyDiv)
+
+//ex 2
+
+const Heading=document.createElement('h2');
+const HeadingText=document.createTextNode('dynamic heading');
+Heading.appendChild(HeadingText)
+Heading.classList.add('blue');
+
+//insertBefore('elemnt','location')
+Element.insertBefore(Heading,FiRst)
+
+//replaceChild('new','old')
+const SmallHeading=document.createElement('h6');
+const SmallHeadingText=document.createTextNode('Small heading....');
+SmallHeading.appendChild(SmallHeadingText)
+SmallHeading.classList.add('red');
+document.body.replaceChild(SmallHeading,bodyDiv)
+
+//2- 
+//prepend
+//innerText
+const HEADING=document.createElement('h1');
+HEADING.innerText="i'm a dynamic heading"
+document.body.prepend(HEADING)
+
+//3- 
+// remove
+//removeChild
+
+const RESULT=document.querySelector('#remove');
+// remove.remove();
+//remove heading from result
+const heading1=RESULT.querySelector('h1');
+RESULT.removeChild(heading1)
+
+//4-
+// innerHTML:The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+const randomvalue="random value"
+const ul=document.createElement('ul');
+ul.innerHTML=`<li class="special last">${randomvalue}</li> <li class="special last">javascript</li>`;
+document.body.appendChild(ul);
+//textContent:The textContent property sets or returns the text content of the specified node
+
+
