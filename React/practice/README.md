40225727863ea778c60c146ecbb474d39811b0e7
Ali <ali.al.helbawi.2017@gmail.com>
2022-02-16 11:26:06 +0200

    List, Key Prop And Spread Operator

--- React/practice/README.md
+++ React/practice/README.md
@@ -1,4 +1,3 @@
-
 # React Js crash documentation
 
 ## <mark>- JSX rules</mark>
@@ -202,7 +201,7 @@ const Author = () => {
 ReactDom.render(<BookList />, document.getElementById("root"))
 ```
 
-## <mark>- Props </mark>
+## <mark>- Props & children</mark>
 
 ```javascript
 function BookList() {
@@ -232,6 +231,99 @@ function Book(props) {
 
   return (
     <article className="book">
+      <Image img={img} />
+      <Title title={title} />
+      <Author author={author} />
+      {props.children}
+    </article>
+  )
+}
+```
+
+## <mark>- List, Key Prop And Spread Operator</mark>
+
+```javascript
+
+const books = [
+
+  {
+    id: 1,
+    title: "How to lose your belly fat",
+    author: "Ali Eh Helbawi",
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpmIZsBWiKrffTcn49P_KYZlo1sjMCb-DX6g&usqp=CAU`
+  }
+  ,
+  {
+    id: 2,
+    title: "How to lose your mind",
+    author: "Ali Eh Helbawi",
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaVQxOgm30Tf1KMvdyGzhr6MkCPImdrJAfnA&usqp=CAU`
+  }
+  ,
+  {
+    id: 3,
+    title: "How to lose your money",
+    author: "Ali Eh Helbawi",
+    img: `https://m.media-amazon.com/images/I/517YcbrSmvS.jpg`
+  }
+]
+
+
+function BookList() {
+  return (
+    <section className='bookList'>
+      {books.map((book) => {
+         const { title, author, img, id } = book
+         return (
+          <Book key={id}
+            title={title}
+            author={author}
+            img={img} />)
+      })}
+
+    </section>
+  )
+}
+
+// or
+function BookList() {
+
+         return (
+          <Book key={book.id}
+            book={book} />
+
+      // in this case we shoud change the book fubction as below:
+      /// we use book.img and so on since now the props is an entry object not destructed yet
+
+      function Book(props) {
+      const { img, title, author } = props
+      return (
+      <article className='book'>
+      <Image img={book.img} />
+      <Title title={book.title} />
+      <Author author={book.author} />
+      </article>
+  )
+}
+      }
+
+////or
+
+function BookList() {
+
+        return (
+          <Book key={book.id}
+            {...book} />)
+  )
+}
+
+
+function Book(props) {
+  // or function Book({ img, title, author, children }) {
+  const { img, title, author } = props
+
+  return (
+    <article className='book'>
       <Image img={img} />
       <Title title={title} />
       <Author author={author} />
--- React/practice/src/index.js
+++ React/practice/src/index.js
@@ -7,31 +7,69 @@ import ReactDom from 'react-dom';
 import './index.css'
 
 // setup variables
-const firstBook = {
-  title: "How to lose your belly fat",
-  author: "Ali Eh Helbawi",
-  img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpmIZsBWiKrffTcn49P_KYZlo1sjMCb-DX6g&usqp=CAU`
-}
+const books = [
 
-const secondBook = {
-  title: "How to lose your mind",
-  author: "Ali Eh Helbawi",
-  img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaVQxOgm30Tf1KMvdyGzhr6MkCPImdrJAfnA&usqp=CAU`
-}
+  {
+    id: 1,
+    title: "How to lose your belly fat",
+    author: "Ali Eh Helbawi",
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpmIZsBWiKrffTcn49P_KYZlo1sjMCb-DX6g&usqp=CAU`
+  }
+  ,
+  {
+    id: 2,
+    title: "How to lose your mind",
+    author: "Ali Eh Helbawi",
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaVQxOgm30Tf1KMvdyGzhr6MkCPImdrJAfnA&usqp=CAU`
+  }
+  ,
+  {
+    id: 3,
+    title: "How to lose your money",
+    author: "Ali Eh Helbawi",
+    img: `https://m.media-amazon.com/images/I/517YcbrSmvS.jpg`
+  }
+]
 
 
 function BookList() {
   return (
     <section className='bookList'>
-      <Book title={firstBook.title} 
-      author={firstBook.author} 
-      img={firstBook.img} >
-        {/* children props mustbe in between the tags */}
-        <a href="#">Pro-Solution.net</a>
-      </Book>
-      <Book title={secondBook.title} 
-      author={secondBook.author} 
-      img={secondBook.img} />
+      {books.map((book) => {
+        {/* const { title, author, img, id } = book
+         return (
+          <Book key={id}
+            title={title}
+            author={author}
+            img={img} />)
+      })} */}
+        /////////////////////////////////////////////
+        // or 
+        {/* return (
+          <Book key={book.id}
+            book={book} />)
+      ***************************
+      // in this case we shoud change the book fubction as below:
+      /// we use book.img and so on since now the props is an entry object not destructed yet
+      ***************************
+      function Book(props) {
+      const { img, title, author } = props
+      return (
+      <article className='book'>
+      <Image img={book.img} />
+      <Title title={book.title} />
+      <Author author={book.author} />
+      </article>
+  )
+}
+      })} */}
+        ////////////////////////////////////////////
+        // or
+        return (
+          <Book key={book.id}
+            {...book} />)
+      })}
+
     </section>
   )
 }
