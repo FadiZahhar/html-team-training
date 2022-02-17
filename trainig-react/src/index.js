481586ba619ad5568367d93e36c124e535eee066
lora lameh <loralameh@gmail.com>
2022-02-17 15:32:09 +0200

    using JS in JSX

--- trainig-react/src/index.js
+++ trainig-react/src/index.js
@@ -1,66 +1,27 @@
 import react from 'react'
 import ReactDOM from 'react-dom'
-
-// css  - we need to import full path cz it is not a dependancy
-//this css method is good for relatively small projects
 import './index.css'
-//Nested component , react tools
-
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  )
-}
-//implicit return example
-const Person = () => <h2> lora</h2>
-
-//implicit return example
-const Message = () => {
-  return <p>this is my message</p>
-}
-
-//****************************************** */
 
 //creating an amazon like book store
 function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 const Book = () => {
+  const title = 'The girl who said NO!'
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Auther />
+      <img
+        src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/561b1483830519.5d48f1ff93014.jpg'
+        alt='book cover'
+      />
+      {/* to access js in jsx use {} */}
+      <h1>{title.toUpperCase()}</h1>
+      <h2>Zoi Tueker</h2>
     </article>
-  )
-}
-const Auther = () => {
-  return (
-    //inline styling
-    <h2 style={{ color: '#617d38', fontSize: '1.25rem', marginTop: '0.25rem' }}>
-      Zoi Tueker
-    </h2>
-  )
-}
-const Title = () => {
-  return <h1> The girl who said NO!</h1>
-}
-const Image = () => {
-  return (
-    <img
-      src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/561b1483830519.5d48f1ff93014.jpg'
-      alt='book cover'
-    />
   )
 }
 
