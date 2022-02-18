c0695ce3cee296bf5e3a4f2eb213dbd2bdfdd464
lora lameh <loralameh@gmail.com>
2022-02-18 16:23:09 +0200

    create a file of data export it and import it in the other file

--- trainig-react/src/BookList.js
+++ trainig-react/src/BookList.js
@@ -1,18 +1,7 @@
 //css
 import './index.css'
+import { book1, book2 } from './data'
 
-//settig up vars
-const book1 = {
-  title: 'The girl who said NO!',
-  auther: 'Zoi Tueker',
-  img: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/561b1483830519.5d48f1ff93014.jpg',
-}
-
-const book2 = {
-  title: 'Hello Hippo!',
-  auther: 'Aallyah S. Peres',
-  img: 'https://pub-static.fotor.com/assets/projects/pages/6b2a8400-1c2b-11e8-9b8a-bbe4815ca77f_f5458c4c-b515-4128-b5cf-0d69cd3c9073_thumb.jpg',
-}
 //creating an amazon like book store
 function BookList() {
   return (
---
+++ trainig-react/src/data.js
@@ -0,0 +1,12 @@
+//settig up vars
+export const book1 = {
+  title: 'The girl who said NO!',
+  auther: 'Zoi Tueker',
+  img: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/561b1483830519.5d48f1ff93014.jpg',
+}
+
+export const book2 = {
+  title: 'Hello Hippo!',
+  auther: 'Aallyah S. Peres',
+  img: 'https://pub-static.fotor.com/assets/projects/pages/6b2a8400-1c2b-11e8-9b8a-bbe4815ca77f_f5458c4c-b515-4128-b5cf-0d69cd3c9073_thumb.jpg',
+}
