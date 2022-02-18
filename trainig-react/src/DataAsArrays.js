1a93bc6dd5323b0c37cc1a043e90ef95c0f993aa
lora lameh <loralameh@gmail.com>
2022-02-18 13:07:09 +0200

    rendering simple array example

--- trainig-react/src/DataAsArrays.js
+++ trainig-react/src/DataAsArrays.js
@@ -1,33 +1,16 @@
 //css
 import './index.css'
 
-//settig up vars
-const books = [
-  {
-    title: 'The girl who said NO!',
-    auther: 'Zoi Tueker',
-    img: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/561b1483830519.5d48f1ff93014.jpg',
-  },
-  {
-    title: 'Hello Hippo!',
-    auther: 'Aallyah S. Peres',
-    img: 'https://pub-static.fotor.com/assets/projects/pages/6b2a8400-1c2b-11e8-9b8a-bbe4815ca77f_f5458c4c-b515-4128-b5cf-0d69cd3c9073_thumb.jpg',
-  },
-]
-//creating an amazon like book store
-function DataAsArrays() {
-  return <section className='booklist'>{books}</section>
-}
+//settig up array
 
-// const Book = (props) => {
-//   const { img, title, auther } = props
-//   return (
-//     <article className='book'>
-//       <img src={img} alt='book cover' />
-//       <h1>{title}</h1>
-//       <h2>{auther}</h2>
-//     </article>
-//   )
-// }
+//small array example on how we access arrays in which map reaches or every single value in the array.
+const names = ['john', 'suzy', 'ayla']
+const newnames = names.map((name) => {
+  console.log(name)
+  return <h1>{name}</h1>
+})
+function DataAsArrays() {
+  return <section className='booklist'>{newnames}</section>
+}
 
 export default DataAsArrays
