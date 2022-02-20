ebf2520ff01abbc4fa6ace61642f64d7c360f454
lora lameh <loralameh@gmail.com>
2022-02-20 12:49:45 +0200

    scroll Event

---
+++ scrollEvent.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  console.log(window.scrollX + 'px')
+  console.log(window.scrollY + 'px')
+})
+
+//window.scrollX returns how many px are scrolled along x axis left or right
+
+//this event is only firedup if we really have somthing to scroll to
