bddcb3a67c0762e8a304a63a6a7891b732cad2f3
lora lameh <loralameh@gmail.com>
2022-02-20 19:52:56 +0200

    UseEffect

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
-// second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setvalue] = useState(0)
+  useEffect(() => {
+    console.log('useEffect')
+    if (value > 0) {
+      document.title = `New Messages (${value})`
+    }
+  })
+  // second parameter
+  useEffect(() => {
+    console.log('runs on inial render only')
+  }, [])
 
-export default UseEffectBasics;
+  // second parameter
+  useEffect(() => {
+    console.log('runs on inial render and upon any changes in the value')
+  }, [value])
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setvalue(value + 1)}>
+        Click me
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
--- react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setsize] = useState(window.innerWidth)
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setsize(window.innerWidth)
+    })
+    //the following retun fireup onses we rerender
+    return () => {
+      window.removeEventListener('resize', () => {
+        setsize(window.innerWidth)
+      })
+    }
+  }, [])
+  return (
+    <>
+      <h1>Size:</h1>
+      <h2>{size}</h2>
+    </>
+  )
+}
 
-export default UseEffectCleanup;
+export default UseEffectCleanup
