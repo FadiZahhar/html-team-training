802e1950aa97e6f07ea04c30fa432f279d7b1195
lora lameh <loralameh@gmail.com>
2022-02-21 15:52:08 +0200

    toggle components

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      {show && <Item />}
+      <button className='btn' onClick={() => setShow(!show)}>
+        {' '}
+        show / hide
+      </button>
+    </>
+  )
+}
 
-export default ShowHide;
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth)
+  const checksize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', checksize)
+    return () => {
+      window.removeEventListener('resize', checksize)
+    }
+  })
+  return (
+    <div>
+      <h1>window</h1>
+      <h2>size:{size} px </h2>
+    </div>
+  )
+}
+export default ShowHide
