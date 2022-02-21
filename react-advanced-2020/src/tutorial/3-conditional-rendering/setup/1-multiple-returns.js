8d83d94153f9beeb249a7f7d2c15ec3bce75f836
lora lameh <loralameh@gmail.com>
2022-02-21 14:54:20 +0200

    multiple returns

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,50 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
 
-export default MultipleReturns;
+  useEffect(() => {
+    //he uses the promise syntex since fetch returns a promise
+    //the default behaivior if fetch is that it does not trogger the erorr on 404 it trigrs it on network error so the turn around here is to explore the retuned json oject befor we proceeed and add it as a user
+    fetch(url)
+      .then((resp) => {
+        //make sure there is no error response
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          console.log('404 ')
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((user) => {
+        //login is a value in the user object so we are distucturing
+        const { login } = user
+        setUser(login)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log('network error')
+      })
+  }, [])
+  if (isLoading) {
+    return (
+      <>
+        <h2>loading...</h2>;
+      </>
+    )
+  }
+  if (isError) {
+    return (
+      <>
+        <h2>Error...</h2>;
+      </>
+    )
+  }
+  return <h2>{user}</h2>
+}
+
+export default MultipleReturns
