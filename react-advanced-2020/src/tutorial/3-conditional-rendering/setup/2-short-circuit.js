9de000aac99bc5122c36d51169175035bc188656
lora lameh <loralameh@gmail.com>
2022-02-21 15:16:58 +0200

    short circuit evaluation

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello worldy'
+  //the way || works in js is that if the first term is falsy it returns the second term other wise it firstValue=text
+  const secondValue = text && 'hello world'
+  //the way && works in JS is that id first term is truthy retun the second one thus secondValue=helloworld else if the first is false retun it
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || 'hello worldy'}</h1>
+      <h1>{text && 'hello world'}</h1>
+    </>
+  )
+}
 
-  return <h2>short circuit</h2>;
-};
-
-export default ShortCircuit;
+export default ShortCircuit
