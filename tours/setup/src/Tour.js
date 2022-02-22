ecfdbec72aed3ddcadd4f227b786193fa3f26a21
lora lameh <loralameh@gmail.com>
2022-02-22 12:57:36 +0200

    add tour component

--- tours/setup/src/Tour.js
+++ tours/setup/src/Tour.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Tour = () => {
-  return <h2>tour component</h2>;
-};
+const Tour = (props) => {
+  const { id, image, info, name, price } = props.tour
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name}></img>
+      <footer>
+        <div classname='tour-info'>
+          <h4> {name}</h4>
+        </div>
+        <div classname='tour-price'>${price}</div>
+        <p>{info}</p>
+        <button className='delete-btn'>Not Interested</button>
+      </footer>
+    </article>
+  )
+}
 
-export default Tour;
+export default Tour
