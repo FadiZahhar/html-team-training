7839df5b343f0bf33fc47cd6e22ac5f37030e518
lora lameh <loralameh@gmail.com>
2022-02-22 13:12:39 +0200

    add toggle functionality of  to display info

--- tours/setup/src/Tour.js
+++ tours/setup/src/Tour.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Tour = (props) => {
   const { id, image, info, name, price } = props.tour
+  const [readMore, setReadMore] = useState(false)
   return (
     <article className='single-tour'>
       <img src={image} alt={name}></img>
@@ -10,7 +11,10 @@ const Tour = (props) => {
           <h4> {name}</h4>
         </div>
         <div classname='tour-price'>${price}</div>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'read Less' : 'read More'}
+        </button>
         <button className='delete-btn'>Not Interested</button>
       </footer>
     </article>
