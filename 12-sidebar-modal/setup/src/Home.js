c1952331933dbf99f4c8937ecfa2fb9e32dac94c
lora lameh <loralameh@gmail.com>
2022-02-25 00:42:51 +0200

    set appContext and its custom hook

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,12 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
-
+import { AppContext } from './context'
+import { useGlobalContext } from './context'
 const Home = () => {
+  // const data = useContext(AppContext)
+  //instead of all of these imports we can use our custom hook
+  const data = useGlobalContext()
+  console.log(data)
   return (
     <main>
       <button className='sidebar-toggle'>
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,40 @@
 import React, { useState, useContext } from 'react'
+import App from './App'
+const AppContext = React.createContext()
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+  const [isModelOpen, setIsModelOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSideBarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSideBarOpen(false)
+  }
+  const openModel = () => {
+    setIsModelOpen(true)
+  }
+  const closeModel = () => {
+    setIsModelOpen(false)
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        openSidebar,
+        closeSidebar,
+        openModel,
+        closeModel,
+        isSideBarOpen,
+        isModelOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+export { AppContext, AppProvider }
