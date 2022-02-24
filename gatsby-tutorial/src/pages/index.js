f1ab3cbc678a5932809b03ea7f57bdad29f7f60f
nourham223 <98809682+nourham223@users.noreply.github.com>
2022-02-24 13:14:54 +0200

    gatsby styled components

--- gatsby-tutorial/src/pages/index.js
+++ gatsby-tutorial/src/pages/index.js
@@ -1,12 +1,31 @@
 import React from "react"
 import Layout from "../components/Layout";
 import * as name from '../styling/index.module.css';
+import {ExampleButton} from './button';
+import styled from "styled-components";
 
 export default function Home() {
   return(
+
   <Layout>
-    
+  <Wrapper>
+
+  <ExampleButton>click me</ExampleButton>
   <h1 className={name.text}>Hello world</h1>
- 
-  </Layout>)
+  <p>My name is Nour</p>
+
+  </Wrapper>
+  </Layout>
+  )
 }
+
+const Wrapper = styled.section`
+color: blue;
+h1{
+  color:yellowgreen ;
+
+}
+.text{
+  text-decoration: overline;
+}
+`
