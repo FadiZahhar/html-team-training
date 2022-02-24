b118028f31e2637430e30225f30278b73a94077b
lora lameh <loralameh@gmail.com>
2022-02-24 17:04:42 +0200

    local storage

--- grocery-bud/setup/src/App.js
+++ grocery-bud/setup/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
-
+const getLocal = () => {
+  let mylist = localStorage.getItem('list')
+  if (mylist) {
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
 function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocal())
   const [isEditting, setIsEditting] = useState(false)
   const [alert, setAlert] = useState({
     show: false,
@@ -60,6 +67,9 @@ function App() {
     setEditID(id)
     setName(specificItem.title)
   }
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
   return (
     <section className='section-center'>
       <form onSubmit={handleSubmit} className='grocery-form'>
