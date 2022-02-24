77220f864034c50db6c46af5c9f107d2db276d34
lora lameh <loralameh@gmail.com>
2022-02-24 23:35:30 +0200

    sidebar structure

---
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+const Modal = () => {
+  return (
+    <div className={`model-overlay `}>
+      <div className='model-container'>
+        <h3>modal content</h3>
+        <button className='close-model-btn'>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
