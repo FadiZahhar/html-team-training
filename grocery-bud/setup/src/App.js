621cb1d5ea593193b288e10f2e25a7adcdf3194c
lora lameh <loralameh@gmail.com>
2022-02-24 14:57:29 +0200

    set alert to show for 3 sec

--- grocery-bud/setup/src/App.js
+++ grocery-bud/setup/src/App.js
@@ -6,27 +6,36 @@ function App() {
   const [name, setName] = useState('')
   const [list, setList] = useState([])
   const [isEditting, setIsEditting] = useState(false)
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const [alert, setAlert] = useState({
+    show: false,
+    type: '',
+    msg: '',
+  })
   const [editID, setEditID] = useState(null) //which item is being editted
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!name) {
-      //display alert
+      showAlert(true, 'danger', 'please enter a value')
     } else if (name && isEditting) {
       //deal with edit
     } else {
-      setAlert({ ...alert, show: true })
+      showAlert(true, 'success', `the item ${name} has been added to your list`)
+
       const newitem = { id: new Date().getTime().toString(), title: name }
       setList([...list, newitem])
       setName('')
     }
   }
 
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg })
+    // a short cut for setAlert(show:show,type:type,msg:msg)
+  }
   return (
     <section className='section-center'>
       <form onSubmit={handleSubmit} className='grocery-form'>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Grocery bub</h3>
         <div className='form-control'>
           <input
