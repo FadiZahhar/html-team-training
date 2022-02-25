bad2c80b4a40a2374f5e9dbd7301e421f61cd94c
lora lameh <loralameh@gmail.com>
2022-02-25 12:42:33 +0200

    React Router - URL Params And Placeholder

--- react-advanced-2020/src/tutorial/11-react-router/setup/Person.js
+++ react-advanced-2020/src/tutorial/11-react-router/setup/Person.js
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { data } from '../../../data'
+import { Link, useParams } from 'react-router-dom'
 const Person = () => {
+  const [name, setName] = useState('h')
+  const { id } = useParams()
+  useEffect(() => {
+    const myname = data.find((person) => person.id === parseInt(id))
+
+    setName(myname.name)
+  }, [])
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to='/people'> go back</Link>
     </div>
-  );
-};
+  )
+}
 
-export default Person;
+export default Person
