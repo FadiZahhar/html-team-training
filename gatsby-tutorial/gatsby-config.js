1ecdadef37a5a46652e910b8c515a992e2a4bc9b
nourham223 <98809682+nourham223@users.noreply.github.com>
2022-02-25 20:44:19 +0200

    gatsby-source-system

--- gatsby-tutorial/gatsby-config.js
+++ gatsby-tutorial/gatsby-config.js
@@ -22,5 +22,12 @@ complexData:[
   plugins: [`gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`],
+    `gatsby-transformer-sharp`,
+  {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },],
 }
