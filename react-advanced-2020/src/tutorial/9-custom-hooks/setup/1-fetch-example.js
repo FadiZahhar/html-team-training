e19210e057848cec3f80f66fce111d9c9e46f493
lora lameh <loralameh@gmail.com>
2022-02-25 11:07:45 +0200

    custom hooks

--- react-advanced-2020/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ react-advanced-2020/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useFetch } from './2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
-  console.log(products)
+  const { loading, data } = useFetch(url)
+  console.log(data)
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
--- react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true)
+  const [data, setdata] = useState([])
+
+  const getdata = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setdata(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getdata()
+  }, [url])
+  return { loading, data }
+}
