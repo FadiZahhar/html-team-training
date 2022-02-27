23f286219b7c4a20c2570dbc9c9fb53c78fb4a1f
nourham223 <98809682+nourham223@users.noreply.github.com>
2022-02-27 12:31:07 +0200

    GatsbyImage

--- gatsby-tutorial/src/pages/gallery.js
+++ gatsby-tutorial/src/pages/gallery.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { useStaticQuery,graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image' 
+import styled from 'styled-components'
 
 export const query = graphql`
   {
     allFile(filter: {extension: {ne: "svg"}}) {
-      edges {
-        node {
-          name
-          childrenImageSharp {
-            gatsbyImageData(
-              layout: CONSTRAINED
-              placeholder: TRACED_SVG
-              transformOptions: {grayscale: true}
-            )
-          }
+      nodes {
+        childImageSharp {
+          gatsbyImageData(placeholder: TRACED_SVG, width: 200, layout: FIXED, height: 200)
         }
+        name
       }
     }
   }
@@ -24,10 +19,32 @@ export const query = graphql`
 function Gallery() {
 
     const data = useStaticQuery(query);
-    console.log(data);
+    const nodes = data.allFile.nodes;
+    
   return (
-    <div>gallery</div>
+    <Wrapper>
+    {nodes.map((image,index) => {
+      const { name } = image
+  return(
+    <article key={index} className='item'>
+      <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={name}
+      className='gallery-img'/>
+      <p>{name}</p>
+    </article>
+    )
+  })}
+  </Wrapper>
   )
 }
 
+const Wrapper = styled.section`
+display: flex;
+flex-wrap:wrap;
+.item{
+  margin: 1rem;
+}
+.gallery-img{
+  border-radius: 1em;
+}
+`
 export default Gallery
