0d0f82b0f3568c8882854ccc7c8c317e9a5f7df5
Ali <ali.al.helbawi.2017@gmail.com>
2022-03-07 23:58:50 +0200

    Setting up Firebase

--- Getting Started with Firebase 9/firebase-9-tutorial/src/App.js
+++ Getting Started with Firebase 9/firebase-9-tutorial/src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { initializeApp } from 'firebase/app'
 
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDFX6D3p3KR_DgEf1d25cSXBMsQpwSkglQ",
   authDomain: "fir-9-pro-solution.firebaseapp.com",
@@ -11,9 +15,28 @@ const firebaseConfig = {
   appId: "1:753852677251:web:93429dece79b7654b56fb6"
 };
 
+// init firabase app
 initializeApp(firebaseConfig)
 
+// init service
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef).then((snapshot) => {
+  let books = []
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(books);
+}).catch(error => {
+  console.log(error.message);
+})
+///
 function App() {
+  const [books, setBooks] = useState('')
 
 
   return (
