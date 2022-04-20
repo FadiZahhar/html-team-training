dfb66b76ca75428c0c6ea7810ca4ca9bce3ddd4d
Fatima-kouta <fatimakouta286@gmail.com>
2022-04-20 15:56:13 +0300

    Creating Context

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import BookList from './components/BookList';
 import Songs from './components/Songs';
+import BookContextProvider from './contexts/BookContext ';
 
 
 function App() {
   return (
     <div className="App">
       <Songs />
+  
+      <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
     </div>
   );
 }
---
+++ contextapp/src/components/BookList.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext ';
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+  return ( 
+    <div className="book-list" >
+      <ul>
+        {books.map(book => {
+          return (
+            <li key={book.id}>{book.title}</li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+ 
+export default BookList;
