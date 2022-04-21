eb1c1989d8d9cc6ac156c9eb97f3d65b3563ffee
lora lameh <loralameh@gmail.com>
2022-04-21 16:52:19 +0300

    use Effect hook

--- react-advanced-2020/src/tutorial/13-context&hooks/useState/UseState-SongList.js
+++ react-advanced-2020/src/tutorial/13-context&hooks/useState/UseState-SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SongListForm from './UseState-forms'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -8,9 +8,13 @@ const SongList = () => {
     { id: 2, title: 'radio' },
     { id: 3, title: 'hello again' },
   ])
+  const [age, setAge] = useState(20)
   const addSong = (title) => {
     setSongs([...songs, { id: uuidv4(), title }])
   }
+  useEffect(() => {
+    console.log(songs)
+  }, [songs])
   return (
     <div className='song-list'>
       <ul>
@@ -19,6 +23,7 @@ const SongList = () => {
         })}
       </ul>
       <SongListForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   )
 }
