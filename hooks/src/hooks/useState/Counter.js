a6cb0c904fc97a097d1af9457339e13c575a39d1
nourham223 <98809682+nourham223@users.noreply.github.com>
2022-04-21 17:32:53 +0300

    useMemo

---
+++ hooks/src/hooks/useState/Counter.js
@@ -0,0 +1,41 @@
+import React, {useReducer} from "react";
+
+function reducer(counter, action) {
+switch(action.type){
+case 'increment':
+     return (counter + 1)
+case 'decrement':
+     return (counter - 1)
+
+default:
+         return counter
+}
+}
+
+export default function Counter() {
+ const [counter, dispatch] = useReducer(reducer, 0);
+
+
+function increment () {
+      dispatch({type:'increment'})
+}
+     
+function decrement () {
+     dispatch({type:'decrement'})
+}
+     
+     
+  return (
+      <>
+    <div className="counter">
+        <span className="example"> useReducer Example:</span>
+        <div className="counter-text">Counter</div>
+    <br/>
+    <div>{counter}</div>
+    <button className="counter-btn" onClick={increment}>Increase</button>
+     <button className="counter-btn" onClick={decrement}>Decrease</button>
+     </div>
+    </>
+  )
+}
+
--- hooks/src/hooks/useState/Home.js
+++ hooks/src/hooks/useState/Home.js
@@ -1,55 +1,94 @@
 import { useState, useEffect, useRef } from "react";
 import BlogList from "./BlogList";
+import Counter from './Counter'
+import ChangeTheme from "./ChangeTheme";
+
 
 const Home = () => {
 
-  const [name2, setName2]= useState('');
+ 
+  const [number, setNumber]= useState('');
+  const [isPending, setIsPending]= useState(true);
 
   const nameInputRef = useRef();
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+
+  const [blogs, setBlogs] = useState([])
+
+  // const [blogs, setBlogs] = useState([ 
+  //    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+  //   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+  //   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  // ])
 
   const [name, setName] = useState('mario');
 
-  const handleDelete =(id) => {
+  const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog=> blog.id !== id);
     setBlogs(newBlogs)
   }
 
+  //   useEffect(() => {
+  //   console.log('use effect ran');
+  //   console.log(blogs);
+  // }, [blogs])
+
+
   useEffect(() => {
-    console.log('useEffect run');
-  },)
+    setTimeout(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(res =>{
+      return res.json();
+    })
+    .then(data => {
+     setIsPending(false);
+      setBlogs(data)
+    })
+     }, 1000);
+  },[])
+
 
 const changeHandler =()=> {
-setName2(nameInputRef.current.value)
+setNumber(nameInputRef.current.value)
   }
 
   return (
+    <>
+    
     <div className="home">
+    { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
        <button onClick={() => setName('luigi')}>change name</button>
        <p>{name}</p>
        <br/>
        <div className="input-style">
+           <span className="example"> useRef Example:</span>
         <label 
-        htmlFor="Name" 
-        className="label" 
-      >
-        ENTER YOUR NAME</label>
-        <input type='text' id='Name' ref={nameInputRef}
+        htmlFor="Number">
+        <p className="label"> ENTER YOUR NUMBER</p>
+       </label>
+        <input 
+        type='text' 
+        id='Number' 
+        ref={nameInputRef}
         />
         
         <div>
           <br/>
-        <button type="submit" onClick={()=>{changeHandler()}}> ADD NAME</button>
+
+        <button type="submit" onClick={()=>{changeHandler()}}> ADD NUMBER</button>
         </div>
 
-        <div>{name2}</div>
+        <div>{number}</div>
     </div>
     </div>
+    <br/>
+    <br/>
+    <br/>
+    <ChangeTheme/>
+    <br/>
+    <br/>
+       <Counter/>
+    </>
   );
 }
  
